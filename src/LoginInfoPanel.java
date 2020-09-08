d1f68025964d78906f742e89bfcb3e4f6d4e2a2d
Abdullah Saydemir <abdullah.saydemir@ozu.edu.tr>
2020-09-08 16:09:21 +0300

    Init commit

--- src/LoginInfoPanel.java
+++ src/LoginInfoPanel.java
@@ -1,4 +1,5 @@
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
