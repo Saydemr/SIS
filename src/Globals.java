750bf4a64fb722176d4d06149f14b4c483a51aad
Abdullah Saydemir <abdullah.saydemir@ozu.edu.tr>
2020-09-23 17:32:52 +0300

    doubleLogin boolean added

--- src/Globals.java
+++ src/Globals.java
@@ -1,6 +1,9 @@
 public class Globals {
     public static String driver = "";
     public static String OsInfo = "";
+    private static String username = "";
+    private static String password = "";
+    public static boolean doubleLogin = false;
 
     public static String getUsername() {
         return username;
@@ -18,7 +21,6 @@ public class Globals {
         Globals.password = password;
     }
 
-    private static String username = "";
-    private static String password = "";
+
 
 }
